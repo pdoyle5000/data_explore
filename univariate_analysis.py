c80812bf804559be3e0cb10106cb9972112a9bc8
pdoyle5000 <pdoyle5000@gmail.com>
2017-11-26 12:11:04 -0500

    messing around more

--- univariate_analysis.py
+++ univariate_analysis.py
@@ -1,6 +1,4 @@
-'''This is a script that imports an xlsx file and performs unvariate analysis.
-pip3 install pandas xlrd numpy'''
-
+#!/usr/bin/env python3
 from __future__ import print_function
 import pandas as pd
 import numpy as np
@@ -14,12 +12,19 @@ FORCE_TYPES = {
     'NumberDogs': np.int64,
     'HomeOwner': np.int64}
 """
+
+def resolve_incompletes(cust_db):
+    #cust_db.Gender.fillnan('Unknown')
+    cust_db.Gender.fillna("Unknown", inplace=True)
+    print(cust_db.Gender.describe())
+    print(list(cust_db))
+
 def main():
     ''' main function that executs on python3 univariate_analysis.py'''
-    customer_db = pd.read_excel('CustomerData_Merrimack.xlsx', na_values="NA")
+   # customer_db = pd.read_excel('CustomerData_Merrimack.xlsx', na_values="NA")
 
     # Split the db into three different data-types for clairty
-    continuous_vars = customer_db.select_dtypes(include=[np.float])
+    '''continuous_vars = customer_db.select_dtypes(include=[np.float])
     integer_vars = customer_db.select_dtypes(include=[np.int64])
     categorical_vars = customer_db.select_dtypes(include=[np.object])
 
@@ -36,6 +41,10 @@ def main():
         print(continuous_vars[key].kurt())
 
     continuous_vars.CreditDebt.hist()
+'''
+
+    customer_db = pd.read_excel('CustomerData_Merrimack.xlsx')
+    complete_db = resolve_incompletes(customer_db)
 
 if __name__ == "__main__":
     main()
