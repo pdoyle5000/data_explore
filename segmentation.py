985987e2a7101f34bef04ed26414015298fad2e0
pdoyle5000 <pdoyle5000@gmail.com>
2017-12-05 20:36:18 -0500

    todo

--- segmentation.py
+++ segmentation.py
@@ -3,15 +3,20 @@ from __future__ import print_function
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from univariate_analysis import FORCE_TYPES
+from univariate_analysis import FORCE_TYPES as forced_types
 from univariate_analysis import OUTPUT_CSV as FROM_ANALYSIS
 
+# Force the rest to categorical
 def import_data():
-    return pd.read_csv(FROM_ANALYSIS, dtype=FORCE_TYPES)
+    forced_types['Gender'] = 'category'
+    forced_types['JobCategory'] = 'category'
+    forced_types['HouseholdIncome'] = 'category'
+    return pd.read_csv(FROM_ANALYSIS, dtype=forced_types)
 
 def main():
-    db = import_data()
-    print(list(db))
+    df = import_data()
+    print(list(df))
+    print(df.dtypes)
 
 if __name__ == "__main__":
     main()
